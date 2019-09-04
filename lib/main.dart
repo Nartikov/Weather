54d398901a703d86bd3aa1faa472be2df5118a41
Alex Nartikov <alex@MacBook-Pro-macosx.local>
2019-09-04 18:10:38 +0300

    login

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'loginscreen.dart';
 import 'welcome.dart';
+import 'splash.dart';
 
 void main() => runApp(
       MaterialApp(
         title: "Заголовок окна",
         routes: <String,WidgetBuilder>{
           "/login":(BuildContext context)=>LoginScreen(),
+          "/welcome":(BuildContext context)=>Welcome(),
         },
-        home: Welcome(),
+        home: SplashScreen(),
       ),
     );
---
+++ lib/splash.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => new _SplashScreenState();
+}
+
+
+class _SplashScreenState extends State<SplashScreen> {
+  startTime() async {
+    var _duration = Duration(seconds: 4);
+    return Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/login');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('images/logo.png'),
+      ),
+    );
+  }
+}
